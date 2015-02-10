4fa1d4ec137915bbc92dd34ea1cecc7ba2b7b7e0
NewbieOrange <yutiancheng1999@gmail.com>
2015-02-10 23:50:06 +0800

    Reformat code.

--- src/com/mengcraft/injector/BannerLogger.java
+++ src/com/mengcraft/injector/BannerLogger.java
@@ -7,28 +7,36 @@ import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class BannerLogger {
-	private final static BannerLogger LOGGER = new BannerLogger();
-
-	private final File file = new File("injector-banner.log");
-	private Writer writer;
-
-	public void log(String in) {
-		try {
-			this.writer = new FileWriter(file, true);
-			this.writer.write(getDateString() + in);
-			this.writer.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	private String getDateString() {
-		return "[" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + "] ";
-	}
-
-	public static BannerLogger getLogger() {
-		return LOGGER;
-	}
-
+public class BannerLogger
+{
+    private final static BannerLogger LOGGER = new BannerLogger();
+    
+    private final File file = new File("injector-banner.log");
+    private Writer writer;
+    
+    public void log(String in)
+    {
+        try
+        {
+            this.writer = new FileWriter(file, true);
+            this.writer.write(getDateString() + in);
+            this.writer.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    
+    private String getDateString()
+    {
+        return "[" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())
+                + "] ";
+    }
+    
+    public static BannerLogger getLogger()
+    {
+        return LOGGER;
+    }
+    
 }
